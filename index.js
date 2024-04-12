6c3b37a9a233d03e48013a032aeabfda8d3f1442
marianaquinteroro <158610384+marianaquinteroro@users.noreply.github.com>
2024-04-11 21:38:15 -0500

    Add files via upload

---
+++ index.js
@@ -0,0 +1,43 @@
+function sendMessage() {
+
+  var userInput = document.getElementById("userInput1");
+  var message = userInput.value.trim();
+
+  if (message !== "") {
+
+    var messageContainer = document.getElementById("chatMessages");
+    var messageElement = document.createElement("div");
+    messageElement.textContent = message;
+    messageElement.classList.add("message");
+    messageElement.style.backgroundColor = "lightblue"
+    messageElement.style.marginTop = "15px"
+    messageElement.style.padding = "8px" 
+    messageElement.style.marginRight = "300px"
+    messageElement.style.fontSize = "20px"
+    messageContainer.appendChild(messageElement);
+    userInput.value = "";
+
+  }
+
+}
+
+function sendMessage2() {
+  var userInput = document.getElementById("userInput2");
+  var message = userInput.value.trim();
+
+  if (message !== "") {
+
+    var messageContainer = document.getElementById("chatMessages");
+    var messageElement = document.createElement("div");
+    messageElement.textContent = message;
+    messageElement.classList.add("message");
+    messageElement.style.backgroundColor = "lightcoral"
+    messageElement.style.marginTop = "15px"
+    messageElement.style.padding = "8px" 
+    messageElement.style.marginRight = "300px"
+    messageElement.style.fontSize = "20px"
+    messageContainer.appendChild(messageElement);
+    userInput.value = "";
+
+  }
+}
